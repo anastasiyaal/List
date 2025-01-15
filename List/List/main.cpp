296f4adad8945a08c6f7143f376b98c79e7b0599
Anastasiia <mekhova.anastasiya@mail.ru>
2025-01-15 15:00:24 +0300

    added project files

---
+++ List/List/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  List
+//
+//  Created by Анастасия Мехова on 15.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
